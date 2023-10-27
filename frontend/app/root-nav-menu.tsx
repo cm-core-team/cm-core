9d524c3960551eff540cff99eccc1ea0987a2ec9
Jude Davis <judedavis92@gmail.com>
2023-10-27 19:06:35 +0100

    fix: context error (ssr)

---
+++ frontend/app/root-nav-menu.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
+export function RootNavMenu(): JSX.Element {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
