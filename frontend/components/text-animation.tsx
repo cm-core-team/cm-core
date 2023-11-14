6481c014f86f560c58c696594af1d3636a8cb37a
j-koziel <jonathankoziel1@gmail.com>
2023-11-14 10:25:20 +0000

    feat: add fade in animation for scripture ref

--- frontend/components/text-animation.tsx
+++ frontend/components/text-animation.tsx
@@ -5,8 +5,9 @@ import { useEffect } from "react";
 import BlinkingCursor from "./blinking-cursor";
 
 export default function TextAnim() {
-  const baseText =
-    "For God is a God not of disorder but of peace. - 1 Corinthians 14:33";
+  // The typing animation was copied from this article:
+  // https://blog.noelcserepy.com/how-i-created-a-typing-text-animation-with-framer-motion
+  const baseText = "For God is a God not of disorder but of peace.";
   const count = useMotionValue(0);
   const rounded = useTransform(count, (latest) => Math.round(latest));
   const displayText = useTransform(rounded, (latest) =>
@@ -16,16 +17,26 @@ export default function TextAnim() {
   useEffect(() => {
     const controls = animate(count, baseText.length, {
       type: "tween", // Not really needed because adding a duration will force "tween"
-      duration: 1,
+      duration: 1.2,
       ease: "easeInOut",
     });
     return controls.stop;
   }, [count]);
 
   return (
-    <span className="text-4xl">
-      <motion.span>{displayText}</motion.span>
-      <BlinkingCursor />
-    </span>
+    <div className="flex flex-col">
+      <span className="text-4xl">
+        <motion.span>{displayText}</motion.span>
+        <BlinkingCursor />
+      </span>
+      <motion.a
+        href="https://wol.jw.org/en/wol/dx/r1/lp-e/1001070150/28803"
+        target="_blank"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
+        1 Corinthians 14:33
+      </motion.a>
+    </div>
   );
 }
