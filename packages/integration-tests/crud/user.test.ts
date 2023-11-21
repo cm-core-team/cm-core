7ecddee42731ed3f25fa89cc280aa90206e1983b
Jude Davis <judedavis92@gmail.com>
2023-11-21 17:54:38 +0000

    fails

--- packages/integration-tests/crud/user.test.ts
+++ packages/integration-tests/crud/user.test.ts
@@ -1,5 +1,7 @@
-import { describe, it } from "bun:test";
+import { describe, expect, it } from "bun:test";
 
 describe("test test", () => {
-  it("should return true", () => {});
+  it("should return true", () => {
+    expect(1).toBe(2);
+  });
 });
