8b6c05350ee06248d249af6ded2f51780cda35c5
Jude Davis <judedavis92@gmail.com>
2023-11-22 15:36:08 +0000

    feat: global standardized user errors

---
+++ backend/internal/common/user_errors.go
@@ -0,0 +1,19 @@
+package common
+
+type UserErrors struct {
+	UserErrKey string
+
+	BadRequestOrData string
+	AuthInvalid      string
+
+	FailedToCreateCongregation string
+}
+
+var UserErrorInstance = UserErrors{
+	UserErrKey: "error",
+
+	BadRequestOrData: "Error on our side. Please report if this persists.",
+	AuthInvalid:      "You are not authorized. Please check your credentials.",
+
+	FailedToCreateCongregation: "Could not create congregation, please try again later and report this error if it exists.",
+}
