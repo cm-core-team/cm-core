5ec8aa93c0c44d15e92f92fb85e2ea11c77512e3
j-koziel <jonathankoziel1@gmail.com>
2023-11-25 17:29:35 +0000

    chore: commit what I have for now

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -12,8 +12,8 @@ type CardProps = React.ComponentProps<typeof Card>;
 
 export default function Page({ className, ...props }: CardProps) {
   return (
-    <div className="flex flex-grow flex-shrink  gap-x-2 h-full">
-      <div className="flex flex-col  w-full">
+    <div className="flex flex-grow flex-shrink gap-x-2 h-screen">
+      <div className="flex flex-col space-y-4 w-full">
         <Card>
           <CardHeader>
             <CardTitle>Your Info</CardTitle>
@@ -51,8 +51,44 @@ export default function Page({ className, ...props }: CardProps) {
           </CardFooter>
         </Card>
       </div>
-      <div className="w-full">
-        <Card>
+      <div className="grid grid-cols-2 w-full">
+        <Card className="w-1/2">
+          <CardHeader>
+            <CardTitle>Your Info</CardTitle>
+            <CardDescription>Everything about you</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+        <Card className="w-1/2">
+          <CardHeader>
+            <CardTitle>Your Info</CardTitle>
+            <CardDescription>Everything about you</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+        <Card className="w-1/2">
+          <CardHeader>
+            <CardTitle>Your Info</CardTitle>
+            <CardDescription>Everything about you</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
+        <Card className="w-1/2">
           <CardHeader>
             <CardTitle>Your Info</CardTitle>
             <CardDescription>Everything about you</CardDescription>
