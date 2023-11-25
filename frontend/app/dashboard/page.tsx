a9bf9a6d5b69219d682e990adcfa28e7223a5a83
j-koziel <jonathankoziel1@gmail.com>
2023-11-25 17:53:04 +0000

    chore: mental clean up

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -30,8 +30,8 @@ type CardProps = React.ComponentProps<typeof Card>;
 
 export default function Page({ className, ...props }: CardProps) {
   return (
-    <div className="flex flex-grow flex-shrink gap-x-2 h-screen">
-      <div className="flex flex-col space-y-4 w-full">
+    <div className="flex flex-grow flex-shrink h-screen">
+      <div className="flex flex-col w-1/2">
         <Card>
           <CardHeader>
             <CardTitle>Your Info</CardTitle>
@@ -104,80 +104,6 @@ export default function Page({ className, ...props }: CardProps) {
                 </DialogFooter>
               </DialogContent>
             </Dialog>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Your Info</CardTitle>
-            <CardDescription>Everything about you</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Your Info</CardTitle>
-            <CardDescription>Everything about you</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-      </div>
-      <div className="grid grid-cols-2 w-full">
-        <Card className="w-1/2">
-          <CardHeader>
-            <CardTitle>Your Info</CardTitle>
-            <CardDescription>Everything about you</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-        <Card className="w-1/2">
-          <CardHeader>
-            <CardTitle>Your Info</CardTitle>
-            <CardDescription>Everything about you</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-        <Card className="w-1/2">
-          <CardHeader>
-            <CardTitle>Your Info</CardTitle>
-            <CardDescription>Everything about you</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-        <Card className="w-1/2">
-          <CardHeader>
-            <CardTitle>Your Info</CardTitle>
-            <CardDescription>Everything about you</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
           </CardFooter>
         </Card>
       </div>
