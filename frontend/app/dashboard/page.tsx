8595297e7d69e6a3e0db682501524115cdb851b7
j-koziel <jonathankoziel1@gmail.com>
2023-11-25 17:49:29 +0000

    feat: create info section

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -6,6 +6,24 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { cn } from "@/lib/utils";
 
 type CardProps = React.ComponentProps<typeof Card>;
@@ -20,10 +38,72 @@ export default function Page({ className, ...props }: CardProps) {
             <CardDescription>Everything about you</CardDescription>
           </CardHeader>
           <CardContent>
-            <p>Card Content</p>
+            <div className="flex flex-col space-y-2">
+              <span>Name: John Doe</span>
+              <span>Email: johndoe@gmail.com</span>
+              <span>Congregation: London, Uxbridge</span>
+            </div>
           </CardContent>
           <CardFooter>
-            <p>Card Footer</p>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="outline">Edit Your Information</Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Edit Your Information</DialogTitle>
+                  <DialogDescription>
+                    Make changes to your information here. Click save when
+                    you&apos;re done.
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      Name
+                    </Label>
+                    <Input
+                      id="name"
+                      defaultValue="John Doe"
+                      className="col-span-3"
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="email" className="text-right">
+                      Email
+                    </Label>
+                    <Input
+                      id="email"
+                      defaultValue="example@gmail.com"
+                      className="col-span-3"
+                      type="email"
+                    />
+                  </div>
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="congregation" className="text-right">
+                      Congregation
+                    </Label>
+                    <HoverCard>
+                      <HoverCardTrigger className="col-span-3">
+                        <Input
+                          id="congregation"
+                          defaultValue="London, Uxbridge"
+                          type="text"
+                          disabled
+                        />
+                      </HoverCardTrigger>
+                      <HoverCardContent>
+                        Your congregation can only be changed by the elders of
+                        the congregation you are in.
+                      </HoverCardContent>
+                    </HoverCard>
+                  </div>
+                </div>
+                <DialogFooter>
+                  <Button type="submit">Save changes</Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </CardFooter>
         </Card>
         <Card>
