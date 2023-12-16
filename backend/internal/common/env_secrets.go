49649de244d7358133e9f52cc56125423495749f
j-koziel <jonathankoziel1@gmail.com>
2023-12-16 21:52:27 +0000

    feat(BREAKING CHANGE): idek anymore

--- backend/internal/common/env_secrets.go
+++ backend/internal/common/env_secrets.go
@@ -1,19 +1,21 @@
 package common
 
-import "os"
+import (
+	"os"
+)
 
 type EnvSecrets struct {
-	DbUrl           string
-	CorsAllowOrigin string
-	JwtSecret       string
-	Environment     string
-	JwAPIEndpoint   string
+	DbUrl                   string
+	CorsAllowOrigin         string
+	JwtSecret               string
+	Environment             string
+	GetWeeklyMeetings_JwAPI string
 }
 
 var EnvSecretsInstance = EnvSecrets{
-	DbUrl:           os.Getenv("DB_URL"),
-	CorsAllowOrigin: os.Getenv("CORS_ALLOW_ORIGIN"),
-	JwtSecret:       os.Getenv("JWT_SECRET"),
-	Environment:     os.Getenv("ENVIRONMENT"),
-	JwAPIEndpoint:   os.Getenv("JW_API_ENDPOINT"),
+	DbUrl:                   os.Getenv("DB_URL"),
+	CorsAllowOrigin:         os.Getenv("CORS_ALLOW_ORIGIN"),
+	JwtSecret:               os.Getenv("JWT_SECRET"),
+	Environment:             os.Getenv("ENVIRONMENT"),
+	GetWeeklyMeetings_JwAPI: os.Getenv("GET_WEEKLY_MEETINGS_JW_API"),
 }
--- backend/internal/integrations/meeting-finder/find_meetings.go
+++ backend/internal/integrations/meeting-finder/find_meetings.go
@@ -21,7 +21,7 @@ type UserLocation struct {
 }
 
 func FindLocalMeetings(location UserLocation, languageCode string) ([]models.Congregation, error) {
-	baseEndpoint := common.EnvSecretsInstance.JwAPIEndpoint
+	baseEndpoint := common.EnvSecretsInstance.GetWeeklyMeetings_JwAPI
 	urlObj, err := url.Parse(baseEndpoint)
 	if err != nil {
 		return nil, err
