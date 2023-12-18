ae63b04efe11976053f81a966e4bc3543caca847
Jude Davis <judedavis92@gmail.com>
2023-12-18 12:17:09 +0000

    feat: branch of jonnies work for login page

--- packages/frontend/app/login/components/login-form.tsx
+++ packages/frontend/app/login/components/login-form.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -17,17 +19,15 @@ import {
 import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-  username: z
-    .string()
-    .min(2, "Your username should have at least 2 characters")
-    .max(50, "Your username should not have more than 20 characters"),
+  email: z.string().email(),
   password: z
     .string()
-    .min(8, "Invalid password")
     .max(50, "Your password should not have more than 50 characters"),
 });
 
 export function LoginForm() {
+  const router = useRouter();
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -42,40 +42,56 @@ export function LoginForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 w-1/3 p-4 mx-auto"
+      >
+        <div>
+          <h1 className="text-2xl font-bold pb-3">Login</h1>
+          <p>
+            Don&apos;t have an account?{" "}
+            <Link href="/register/user">
+              <strong>Register</strong>
+            </Link>
+          </p>
+        </div>
+
         <FormField
           control={form.control}
-          name="username"
+          name="email"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="Username" {...field} />
-              </FormControl>
+              <FormLabel>Email</FormLabel>
               <FormDescription>
-                This is your public display name.
+                The email that you signed up with.
               </FormDescription>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
+
         <FormField
           control={form.control}
           name="password"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Password</FormLabel>
+              <FormDescription>
+                Your existing password to log in.
+              </FormDescription>
               <FormControl>
                 <Input placeholder="Password" type="password" {...field} />
               </FormControl>
-              <FormDescription>
-                This is the password that you set when you made your account.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" className="flex ml-auto">
+          Submit
+        </Button>
       </form>
     </Form>
   );
