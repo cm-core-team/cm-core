df63d5660c411c672d01b8a3a2320061b8eb69d2
Jude Davis <judedavis92@gmail.com>
2024-01-14 21:30:34 +0000

    fix: build

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,8 +1,15 @@
 "use client";
 
+import React from "react";
+
 import { useRouter } from "next/navigation";
 
 export default function Page() {
   const router = useRouter();
-  router.push("/home");
+
+  React.useEffect(() => {
+    router.push("/home");
+  }, [router]);
+
+  return <></>;
 }
