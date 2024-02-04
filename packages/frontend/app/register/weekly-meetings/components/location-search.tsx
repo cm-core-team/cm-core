4c6a1c03a1914b2f51353a43de340fd7d7a325be
j-koziel <jonathankoziel1@gmail.com>
2024-02-04 19:28:19 +0000

    feat: display the possible locations

--- packages/frontend/app/register/weekly-meetings/components/location-search.tsx
+++ packages/frontend/app/register/weekly-meetings/components/location-search.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import React from "react";
+
+import { Button } from "@nextui-org/button";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
@@ -21,6 +25,7 @@ import { LocationSearchFormData } from "@/lib/types/location-form";
 
 export function LocationSearch() {
   const form = useForm<LocationSearchFormData>();
+  const [locationData, setLocationData] = React.useState<any | never>([]);
   const state = useSelector((state: RootState) => state.localMeetings);
   const dispatch: AppDispatch = useDispatch();
 
@@ -32,6 +37,8 @@ export function LocationSearch() {
 
     console.log(geoCodingRes.data.locations);
 
+    setLocationData([...geoCodingRes.data.locations]);
+
     // dispatch(
     //   fetchMeetingsThunk({
     //     latitude: String(geoCodingRes.data.lat),
@@ -41,23 +48,51 @@ export function LocationSearch() {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="query"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Look for a location...</FormLabel>
-              <FormDescription>Find a specific location</FormDescription>
-              <FormControl>
-                <Input placeholder="Street name, postcode or city" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      </form>
-    </Form>
+    <div className="w-full flex flex-col gap-y-3">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="query"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Look for a location...</FormLabel>
+                <FormDescription>Find a specific location</FormDescription>
+                <FormControl>
+                  <Input
+                    placeholder="Street name, postcode or city"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+      {locationData && (
+        <div className="w-full h-1/2 overflow-scroll">
+          {locationData.map((location: any, i: number) => (
+            <Card key={i} className="">
+              <CardHeader>
+                <h2 className="font-bold">{location.formatted}</h2>
+              </CardHeader>
+              <CardBody>
+                <div>
+                  <p>{location.components.city}</p>
+                  <p>{location.components.region}</p>
+                  <p>{location.components.country}</p>
+                </div>
+              </CardBody>
+              <CardFooter>
+                <Button variant="ghost" color="default">
+                  Select
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
